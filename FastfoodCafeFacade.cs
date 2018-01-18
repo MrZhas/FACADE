e6970c31375a168b52146c8e26409819ed3ef1a9
MrZhas <32016417+MrZhas@users.noreply.github.com>
2018-01-18 22:00:09 +0600

    Add files via upload

---
+++ FastfoodCafeFacade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class FastfoodCafeFacade
+    {
+        Sauce sauce;
+        Veggies veggies;
+        Cutlet cutlet;
+        Bun bun;
+        Burger burger;
+
+      
+
+
+
+        public FastfoodCafeFacade(Sauce sa, Veggies ve, Cutlet cu, Bun bun, Burger bur)
+        {
+            this.sauce = sa;
+            this.veggies = ve;
+            this.cutlet = cu;
+            this.bun = bun;
+            this.burger = bur;
+        }
+        public void Start()
+        {
+            sauce.Mix();
+            veggies.ChopCucumber();
+            veggies.ChopOnion();
+            veggies.ChopTomato();
+            cutlet.Barbecue();
+            bun.Slit();
+            bun.Fry();
+        }
+        public void Stop()
+        {
+            burger.SqueezeBun();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sauce sauce = new Sauce();
+            Veggies veggies = new Veggies();
+            Cutlet cutlet= new Cutlet();
+            Bun bun= new Bun();
+            Burger burger= new Burger();
+            FastfoodCafeFacade ffc = new FastfoodCafeFacade(sauce, veggies, cutlet, bun, burger);
+            Cook cook = new Cook();
+            cook.CreateBurger(ffc);
+
+            Console.Read();
+        }
+    }
+    
+}
